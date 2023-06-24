5a46d63fa1a5df3554d33d3b521a0f36c48cdf0d
SandaruwanDS <dsrgotest@gmail.com>
2023-06-25 03:15:14 +0530

    insert user_id coluomn to the Appo table

--- database/migrations/2023_02_19_101834_create_appos_table.php
+++ database/migrations/2023_02_19_101834_create_appos_table.php
@@ -15,6 +15,7 @@ class CreateApposTable extends Migration
     {
         Schema::create('appos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('email');
             $table->string('phone');
