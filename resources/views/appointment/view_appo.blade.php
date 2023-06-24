fbca3d5b67246ca397db76c5ef55e0c1fad43ed3
SandaruwanDS <dsrgotest@gmail.com>
2023-06-25 03:12:11 +0530

    Update view_appo.blade.php

--- resources/views/appointment/view_appo.blade.php
+++ resources/views/appointment/view_appo.blade.php
@@ -1,40 +1,33 @@
 @extends('layouts.navbar3')
 @section('content')
 
-
 <div class="container mt-5">
-
     <div class="row justify-content-center  ">
         <div class="col-md-12 mt-5">
-
             <div class="card">
-                {{-- @if (session('done'))
-                <div class="alert alert-sucsess text-center" role="alert">
-                    {{session('done')}}
-                </div>
-                @endif --}}
 
                 @if (session('delete'))
                 <div class="alert alert-danger text-center" role="alert">
-                    {{session('delete')}}
+                    {{session('delete')}} &#10004;
                 </div>
                 @endif
 
                 @if (session('update'))
                 <div class="alert alert-success text-center" role="alert">
-                    {{session('update')}}
+                    {{session('update')}} &#10004;
                 </div>
                 @endif
 
                 @if (session('already'))
                 <div class="alert alert-primary text-center" role="alert">
-                    {{session('already')}}
+                    {{session('already')}} &#10071;
                 </div>
                 @endif
 
                 <div class="card-header ">
-                    <h3>View Appointments</h3>
+                    <h3>View Appointments
                     <a href="/admin" class="btn btn-outline-primary" style="float:right;">Back to Dashbord</a>
+                     </h3>   
                 </div>
 
                 <div class="card-body">
@@ -60,7 +53,6 @@
                                 <td>{{$data->id}}</td>
                                 <td>{{$data->name}}</td>
                                 <td>{{$data->email}}</td>
-                                
                                 <td>{{$data->phone}}</td>
                                 <td>{{$data->date}}</td>
                                 <td>{{$data->department}}</td>
@@ -71,10 +63,9 @@
                                     <span style="color: rgb(7, 248, 7)">Confirmed</span>
                                     @else
                                     <span style="color: rgb(248, 244, 1)">Waiting...</span> 
-                                    @endif
-                                    
+                                    @endif   
                                 </td>
-                                <td><a href="/update__appointment/{{$data->id}}" class="btn btn-success">Confirm</a></td>
+                                <td><a href="/confirm_appointment/{{$data->id}}" class="btn btn-success">Confirm</a></td>
                                 <td><a href="/delete_appointment/{{$data->id}}" class="btn btn-danger">Delete</a></td>
                             </tr>
                             @endforeach
