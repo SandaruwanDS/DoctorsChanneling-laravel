569c02db27ec2b515490f1db5cab6d8f59a2d1a2
SandaruwanDS <dsrgotest@gmail.com>
2023-06-25 03:10:36 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\models\appo;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,8 +18,26 @@ Route::get('/', function () {
 });
 
 Route::get('/admin',['middleware'=>'isadmin', function () {
-    return view('dashboard');
+    $total = Appo::count();
+    $pending = Appo::where('status', 0)->count();
+    $confirmed = Appo::where('status', 1)->count();
+
+    // Calculate percentages
+    $pendingPercentage = ($pending / $total) * 100;
+    $confirmedPercentage = ($confirmed / $total) * 100;
+    
+    
+    $data = [
+        'total' => $total,
+        'pending' => $pending,
+        'confirmed' => $confirmed,
+        'pendingPercentage' => $pendingPercentage,
+        'confirmedPercentage' => $confirmedPercentage,
+    ];
+
+    return view('dashboard', $data);
 }]);
+
 Route::get('/user',['middleware'=>'isuser', function () {
     return view('home');
 }]);
@@ -27,21 +45,39 @@ Route::get('/user',['middleware'=>'isuser', function () {
 
 
 
-Auth::routes();
+Auth::routes(); 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-
+//Routes for User
 //route for return Create Appointment View
 Route::get('/create_appointment', [App\Http\Controllers\appointment::class, 'returnCreateAppointmentView']);
 //route for return store appointment Details
 Route::post('/store_appointment', [App\Http\Controllers\appointment::class, 'storeAppointmentDetails']);
 //route for return done appointment 
 Route::get('/done_appointment', [App\Http\Controllers\appointment::class, 'doneAppointment']);
+//route for view appointments in user
+Route::get('/show_appointment/{id}', [App\Http\Controllers\appointment::class, 'showAppointment']);
+//route for delete appointments by user 
+Route::get('/delete_appointment_by_user/{id}', [App\Http\Controllers\appointment::class, 'deleteAppointmentByUser']);
+//route for edit appointments by user 
+Route::post('/edit_appointment/{id}', [App\Http\Controllers\appointment::class, 'editAppointment']);
+//route for view edit appointments form
+Route::get('/view_appointment_edit_form/{id}', [App\Http\Controllers\appointment::class, 'viewAppointmentEditForm']);
+//route for update appointments
+Route::post('/update_appointment/{id}', [App\Http\Controllers\appointment::class, 'updateAppointment']);
+
+
+
+
+//Routes for Admin
 //route for view appointments 
 Route::get('/view_appointment', [App\Http\Controllers\appointment::class, 'viewAppointment']);
 //route for delete appointments 
 Route::get('/delete_appointment/{id}', [App\Http\Controllers\appointment::class, 'deleteAppointment']);
 //route for edit appointments 
-Route::get('/update__appointment/{id}', [App\Http\Controllers\appointment::class, 'updateAppointment']);
+Route::get('/confirm_appointment/{id}', [App\Http\Controllers\appointment::class, 'confirmAppointment']);
+// genarete reports
+Route::get('/generate_report/{type}', [App\Http\Controllers\DashboardController::class, 'generateReport']);
+
+
